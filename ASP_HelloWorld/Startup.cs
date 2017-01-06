d7fdd23144d1014dc7e5324f856ad6f38c1c607e
Dhenarra Erine Aisyahsari <dhenarra18@gmail.com>
2017-01-06 22:08:37 +0700

    Add project files.

---
+++ ASP_HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_HelloWorld.Startup))]
+namespace ASP_HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
